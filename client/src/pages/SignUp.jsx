e38bd9a25216614224bbecb095b629d991583cb9
Harsh Sharma <harshsh6935@gmail.com>
2024-08-31 01:41:36 +0530

    complete the sign in functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import {Link} from "react-router-dom";
+import {Link,useNavigate} from "react-router-dom";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  
   const handleChange = (e) =>
   {
     setFormData({ ...formData, [e.target.id]: e.target.value})
@@ -35,6 +37,7 @@ const SignUp = () => {
       setError(true);
       return;
     }
+    navigate('/sign-in');
     }
     catch(err)
     {
